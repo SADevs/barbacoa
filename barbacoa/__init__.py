c1976e2dda0df38a0b2c5da3c40ad1906cbb1447
Daniel Wallace <danielwallace@gtmanfred.com>
2018-12-07 09:42:03 -0800

    add flit for packaging

--- barbacoa/__init__.py
+++ barbacoa/__init__.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+import json
+import os
+import subprocess
+
+version_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'version.json')
+
+if os.path.isfile(version_file):
+    with open(version_file, 'r') as vfile:
+        version_data = json.load(vfile)
+else:
+    version_data = {'version': subprocess.run(['git', 'describe']).stdout.strip()}
+    with open(version_file, 'w') as vfile:
+        json.dump(version_data, vfile)
+
+__version__ = version_data['version']
